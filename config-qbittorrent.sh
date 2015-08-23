09fa694fa0008b399ad9295678933bbfe50dd2dc
Simon Sasburg <simon.sasburg@gmail.com>
2015-08-23 22:09:36 +0200

    Make arm(?) fixes/hacks optional so we still work on desktop ubuntu.

--- config-qbittorrent.sh
+++ config-qbittorrent.sh
@@ -5,4 +5,5 @@ export CPPFLAGS=-DBOOST_ASIO_SEPARATE_COMPILATION
 export libtorrent_CFLAGS=-I${CMAKE_CURRENT_BINARY_DIR}/libtorrent-prefix/include
 export libtorrent_LIBS=${CMAKE_CURRENT_BINARY_DIR}/libtorrent-prefix/lib/libtorrent-rasterbar.a\ -lssl\ -lm\ -lcrypto
 
-./configure --disable-gui --with-libtorrent-rasterbar0.16 --with-boost-libdir=/usr/lib/arm-linux-gnueabihf --prefix=${CMAKE_CURRENT_BINARY_DIR}/qbittorrent-prefix
+shift
+./configure "$@"
